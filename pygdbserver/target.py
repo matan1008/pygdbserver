9bbfc80c6e119e17992c8aad5ecb3bb211d4007b
Matan Perelman <matan1008@gmail.com>
2018-05-07 21:17:53 +0300

    resume, wait, fetch_registers abstract methods added

--- pygdbserver/target.py
+++ pygdbserver/target.py
@@ -67,8 +67,39 @@ class Target:
     @abstractmethod
     def thread_alive(self, pid):
         """
-        Return true if the thread with process ID PID is alive.
+        Return true iff the thread with process ID PID is alive.
         :param pid: The process ID.
-        :return: Whether the process ia alive or not.
+        :return: Whether the process is alive or not.
         """
         return False
+
+    @abstractmethod
+    def resume(self, resume_info):
+        """
+        Resume the inferior process.
+        :param resume_info: List of resume actions.
+        """
+        pass
+
+    @abstractmethod
+    def wait(self, ptid, status, options):
+        """
+        Wait for the inferior process or thread to change state. Store status through argument status.
+        :param ptid: ptid = -1 to wait for any pid to do something,
+        PTID(pid,0,0) to wait for any thread of process pid to do something.
+        :param status: For storing status.
+        :param options: A bit set of options defined as TARGET_W*.
+        If options contains TARGET_WNOHANG and there's no child stop to report,
+        return is null_ptid/TARGET_WAITKIND_IGNORE.
+        :return: Return ptid of child, or -1 in case of error.
+        """
+        return None
+
+    @abstractmethod
+    def fetch_registers(self, regcache, regno):
+        """
+        Fetch registers from the inferior process.
+        :param regcache: Registers cache.
+        :param regno: If regno is -1, fetch all registers; otherwise, fetch at least regno.
+        """
+        pass
