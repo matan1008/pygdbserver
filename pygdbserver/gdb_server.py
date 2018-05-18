54822978fa7d397a703ae7be9851c3aab83a5d84
Matan Perelman <matan1008@gmail.com>
2018-05-18 10:14:27 +0300

    Constructing a response and a basic handling mechanism added

--- pygdbserver/gdb_server.py
+++ pygdbserver/gdb_server.py
@@ -4,6 +4,8 @@ from pygdbserver.pygdbserver_exceptions import *
 class GdbServer(object):
     def __init__(self, target):
         self.target = target
+        self.requests_to_handle_functions_map = {
+        }
 
     @staticmethod
     def calc_checksum(st):
@@ -30,5 +32,26 @@ class GdbServer(object):
             raise InvalidPacketError("Wrong checksum")
         return packet[1:-3]
 
+    @staticmethod
+    def construct_packet(packet_data):
+        """
+        Calculate data's checksum and construct a valid remote gdb packet.
+        :param str packet_data: Packet's data.
+        :return: GDB packet.
+        :rtype: str
+        """
+        return "${}#{:X}".format(packet_data, GdbServer.calc_checksum(packet_data))
+
     def process_packet(self, data):
+        """
+        Handle receiving packet.
+        :param str data: Remote GDB request packet.
+        :return: Remote GDB response packet.
+        :rtype: str
+        """
         packet_data = self.extract_packet_data(data)
+        if packet_data[0] in self.requests_to_handle_functions_map:
+            response = self.requests_to_handle_functions_map[packet_data[0]](packet_data)
+        else:
+            response = ""
+        return self.construct_packet(response)
