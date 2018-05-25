e0595cb3b5f6bc6497e70cc9b8df2434ec51ff3b
Matan Perelman <matan1008@gmail.com>
2018-05-25 17:02:53 +0300

    ThreadResume class added

---
+++ pygdbserver/thread_resume.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+""" Module for ThreadResume class. """
+
+
+class ThreadResume(object):
+    """
+    Describes how to resume a particular thread (or all threads) based on the client's request.
+    If thread is -1, then this entry applies to all threads.
+    These are passed around as an array.
+    """
+
+    def __init__(self, thread):
+        self.thread = thread
+        self.kind = None
+        self.sig = None
+        self.step_range_start = 0
+        self.step_range_end = 0
